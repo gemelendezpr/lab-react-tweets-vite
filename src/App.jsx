724606b5aadc8f4a940170a052a9dba01a8f2150
gerarmc <gerardomelendezpro@gmail.com>
2023-12-01 19:14:29 -0400

    Lab complete

--- src/App.jsx
+++ src/App.jsx
@@ -40,9 +40,12 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet,index)=>(
+        <Tweet key={index} tweet={tweet}/>
+      ))}
     </div>
   );
 }
 
 export default App;
+
